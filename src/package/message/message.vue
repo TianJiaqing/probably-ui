a4113fe25e02690a1158cf0585fd95106629aa49
田家庆 <915326916@qq.com>
2024-08-01 15:23:35 +0800

    内容开发：message弹窗开发，工具函数初始化

--- src/package/message/message.vue
+++ src/package/message/message.vue
@@ -1,111 +1,130 @@
 <template>
-	<div class="message" ref="message" :style="props._style">
-		<div class="animate__animated animate__rotateInDownLeft">
-			<span v-if="props._title"><span class="iconfont" :class="classtype"></span>{{
-				props._title
-			}}</span>
-			<slot v-else></slot>
-		</div>
-	</div>
+  <div class="message animation" ref="message" :style="props._style">
+    <div class="animate-top">
+      <span v-if="props._title"
+        ><span class="iconfont" :class="classtype"></span
+        >{{ props._title }}</span
+      >
+      <slot v-else></slot>
+    </div>
+  </div>
 </template>
 
 
 <script setup>
 import { ref, onMounted, computed } from "vue";
 const props = defineProps({
-	_type: {
-		default: 'warning',
-		type: String
-	},
-	_style: {
-		default: () => ({}),
-		type: Object
-	},
-	_title: {
-		default: '',
-		type: String
-	},
-})
-const _defaut = props.modelValue
-const emit = defineEmits([
-	'_message', 'update:modelValue'
-])
-const _emit = (e) => {
-}
-const message = ref(null)
+  _type: {
+    default: "success",
+    type: String,
+  },
+  _style: {
+    default: () => ({}),
+    type: Object,
+  },
+  _title: {
+    default: "",
+    type: String,
+  },
+  //不推荐时长小于1000
+  _duration: {
+    default: 1000,
+    type: [Number, String],
+  },
+});
+const _defaut = props.modelValue;
+const emit = defineEmits(["_message", "update:modelValue"]);
+const _emit = (e) => {};
+const message = ref(null);
 onMounted(() => {
-	emit('_message', true)
-	const _promise = new Promise((succ, fail) => {
-		setTimeout(() => {
-			message.value.classList.add('remove')
-			succ(null)
-		}, 2000);
-	})
-	_promise.then((res) => {
-		setTimeout(() => {
-			message.value.remove()
-			emit('_message', false)
-		}, 1000)
-	})
-})
-// icon-shibai1
+  emit("_message", true);
+  init();
+});
 
+const class_obj = {
+  success: {
+    class: "icon-chenggong",
+    bg: "rgba(0, 128, 0, .1)",
+  },
+  warning: {
+    class: "icon-jinggao",
+    bg: "rgba(240,160,32,.1)",
+  },
+  fail: {
+    class: "icon-shibai1",
+    bg: "rgba(255,0,0,.1)",
+  },
+};
+const classtype = ref(null);
 
-const classObj = {
-	success: 'icon-chenggong',
-	warning: 'icon-jinggao',
-	fail: 'icon-shibai1'
-}
-const classtype = computed(() => {
-	if (classObj[props._type]) {
-		return classObj[props._type]
-	} else {
-		return 'icon-jinggao'
-	}
-})
+const init = () => {
+  const _promise = new Promise((succ, fail) => {
+    setTimeout(() => {
+      message.value.classList.remove("animation");
+      message.value.classList.add("remove");
+      succ(null);
+    }, props._duration);
+  });
+
+  //   移除元素
+  _promise.then((res) => {
+    setTimeout(() => {
+      message.value.remove();
+      emit("_message", false);
+    }, 1000);
+  });
+  let info = class_obj[props._type];
+  if (info) {
+    message.value.style.cssText += `--t-bg:${info["bg"]};`;
+    classtype.value = info["class"];
+  } else {
+    classtype.value = "icon-jinggao";
+  }
+};
 </script>
 
 <style scoped lang="scss">
 .message {
-	position: fixed;
-	top: 20px;
-	left: 50%;
-	transform: translateX(-50%);
+  position: fixed;
+  bottom: 20px;
+  left: 50%;
 
-	>div {
-		border: 1px solid #E0E0E6;
-		background-color: white;
+  &.animation {
+    animation: animate-top 0.5s ease-in-out forwards;
+  }
 
-		padding: 5px 30px;
-		cursor: pointer;
-		border-radius: 25px;
-		font-size: 14px;
+  > div {
+    background-color: var(--t-bg);
+    color: black;
+    min-width: 400px;
+    padding: 0.6em 2em;
+    cursor: pointer;
+    border-radius: 5px;
+    .iconfont {
+      margin: 0px 5px;
+    }
 
-		.iconfont {
-			margin: 0px 5px;
-		}
+    .icon-shibai1 {
+      color: red;
+    }
 
-		.icon-shibai1 {
-			color: red;
-		}
+    .icon-chenggong {
+      color: green;
+    }
 
-		.icon-chenggong {
-			color: green;
-		}
-
-		.icon-jinggao {
-			color: #F0A020;
-		}
-	}
-
-
-
+    .icon-jinggao {
+      color: #f0a020;
+    }
+  }
 }
 
-.warning {}
+.warning {
+}
 
 .remove {
-	opacity: 0;
-	transition: all .5s linear;
+  //   opacity: 0 !important;
+  animation: opacity 0.2s ease-in-out forwards;
+  top: 1%;
+  transform: translateX(-50%) scale(1);
 }
 </style>
